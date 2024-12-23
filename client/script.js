8d864056dda6761445be98423a23c992a761d660
Prakhar Gupta <guptaprakhar.official1728@gmail.com>
2024-12-23 16:08:31 +0530

    Update button added

--- client/script.js
+++ client/script.js
@@ -4,17 +4,43 @@ const BE_BASE_URL = "http://127.0.0.1:5000";
 const submitBtn = document.getElementById("btn-submit");
 const resetBtn = document.getElementById("btn-reset");
 const newItemTxtField = document.getElementById("text");
-const displayListEl = document.getElementById("task-list")
+const displayListEl = document.getElementById("task-list");
+
+let latestList = [];
+
+function setLatestList(list) {
+    latestList = list.map(el => ({ value: el, editable: false}));
+}
 
 submitBtn.addEventListener("click", handleSubmit);
+newItemTxtField.addEventListener("keydown",function(event) {
+    if (event.key === 'Enter') {
+        handleSubmit();
+    }
+});
 resetBtn.addEventListener("click", handleReset);
+function onWindowLoad() {
+    fetch(
+        BE_BASE_URL + "/get"
+    )
+    .then(response => response.json()) // Converts the ReadableStream into JSON
+    .then(data => {
+        setLatestList(data);
+        emptyAndPopulateList();
+    })
+    .catch(err => window.alert(err));
+};
+onWindowLoad();
 
 function handleSubmit() {
-    addToList(newItemTxtField.value);
+    if (newItemTxtField.value=="") window.alert("Please enter a value")
+    else addToList(newItemTxtField.value);
 }
 
 function handleReset() {
-    resetList();
+    if(window.confirm("This action will delete all your tasks. Continue ? ")) {
+        resetList();
+    }
 }
 
 // CRUD functions
@@ -31,8 +57,10 @@ function addToList(listItem) {
     )
     .then(response => response.json()) // Converts the ReadableStream into JSON
     .then(data => {
-        emptyAndPopulateList(data);
-    });
+        setLatestList(data);
+        emptyAndPopulateList();
+    })
+    .catch(err => window.alert(err));
 }
 
 function resetList() {
@@ -49,15 +77,104 @@ function resetList() {
     .then(() => {
         displayListEl.innerHTML = "";
     })
+    .catch(err => window.alert(err));
 }
 
-function emptyAndPopulateList(list) {
+function emptyAndPopulateList() {
     // Emptying the list
     displayListEl.innerHTML = "";
 
     let htmlStr = "";
-    list.forEach(item  => htmlStr += `<li>${item}</li>` );
+    latestList.forEach(
+        (item,index)  => htmlStr += `
+            <li>
+                <div>
+                    ${
+                        item.editable
+                            ? `<input value="${item.value}" id="new-edit-${index}"/>`
+                            : `${item.value}`
+                    }
+                </div>
+                <div>
+                    ${
+                        item.editable
+                            ? `
+                                <button onclick="saveEditChanges(${index})">
+                                    UPDATE
+                                </button>
+                                <button onclick="cancelEdit(${index})">
+                                    CANCEL
+                                </button>
+                              `
+                            : `<button onclick="edit_Element(${index})" style="border:none">
+                                    <img src="./assets/images/edit.svg" height="20" width="20" />
+                                </button>
+                                <button onclick="delete_Element(${index})" style="border:none">
+                                    <img src="./assets/images/delete.png" height="20" width="20" />
+                                </button>
+                              `
+                    }
+                </div>
+            </li>
+        ` );
     displayListEl.innerHTML = htmlStr;
 }
 
+function delete_Element(index){
+    fetch(
+        BE_BASE_URL + "/del",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: index
+        }
+    )
+    .then(response => response.json()) // Converts the ReadableStream into JSON
+    .then(data => {
+        setLatestList(data);
+        emptyAndPopulateList();
+    })
+    .catch(err => window.alert(err));
+}
 
+function cancelEdit(index) {
+    latestList[index].editable = false;
+    emptyAndPopulateList();
+}
+
+function saveEditChanges(index) {
+    const newValue = document.getElementById(`new-edit-${index}`).value;
+
+    if(newValue.trim().length < 1) {
+        window.alert("Please enter task.")
+        return;
+    }
+
+    fetch(
+        BE_BASE_URL + "/edit",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                index: index,
+                value: newValue,
+            })
+        }
+    )
+    .then(response => response.json()) // Converts the ReadableStream into JSON
+    .then(data => {
+        setLatestList(data);
+        emptyAndPopulateList(data);
+    })
+    .catch(err => window.alert(err));
+
+}
+
+function edit_Element(index){
+    latestList[index].editable = true;
+    emptyAndPopulateList();
+}
--- server/main.py
+++ server/main.py
@@ -18,6 +18,9 @@ start=0
 @app.route('/add', methods=['POST'])
 def submit_data():
     data = request.data.decode('utf-8')
+    if data=='':
+        raise Exception("Empty field !")
+        
     list.append(data)
     return list[start:]
 
@@ -26,5 +29,22 @@ def reset_start():
     global start
     start=len(list)
     return []
+
+@app.route('/get', methods=['GET'])
+def history():
+    return list[start:]
+
+@app.route('/del', methods=['POST'])
+def delete():
+    index= int(request.data.decode('utf-8'))
+    list.pop(index)
+    return list
+
+@app.route('/edit', methods=['POST'])
+def edit():
+    data=request.json
+    index= int(data['index'])
+    list[index]=data['value']
+    return list[start:]
 
 app.run(debug=True)
